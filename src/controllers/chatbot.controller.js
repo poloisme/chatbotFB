9a6768b19025ace41069da7a0e24b8b377890102
nghiatran <trannghia08032000@gmail.com>
2021-11-29 15:56:11 +0700

    update

--- src/controllers/chatbot.controller.js
+++ src/controllers/chatbot.controller.js
@@ -4,7 +4,34 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 const VERIFY_TOKEN = process.env.WEBHOOK_VERIFY_TOKEN;
 const request = require("request");
 const chatbotService = require("../services/chatbot.service");
-const callSendAPI = require("../utils/callSendAPI");
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
 
 const getWebhook = (req, res, next) => {
   // Parse the query params
--- src/services/chatbot.service.js
+++ src/services/chatbot.service.js
@@ -2,7 +2,34 @@ const request = require("request");
 const dotenv = require("dotenv");
 dotenv.config();
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const callSendAPI = require("../utils/callSendAPI");
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {
+  // Construct the message body
+  let request_body = {
+    recipient: {
+      id: sender_psid,
+    },
+    message: response,
+  };
+
+  // Send the HTTP request to the Messenger Platform
+  request(
+    {
+      uri: "https://graph.facebook.com/v2.6/me/messages",
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: "POST",
+      json: request_body,
+    },
+    (err, res, body) => {
+      if (!err) {
+        console.log("message sent!");
+      } else {
+        console.error("Unable to send message:" + err);
+      }
+    }
+  );
+}
 
 const handleGetStarted = (sender_psid) => {
   return new Promise(async (resolve, reject) => {
